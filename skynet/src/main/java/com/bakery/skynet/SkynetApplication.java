6b7a77dd013d8ca26947e4e33bd280bf63c10657
yehorbk <yehor.bk@gmail.com>
2023-04-29 18:41:11 +0300

    swagger-ui configured

--- skynet/src/main/java/com/bakery/skynet/SkynetApplication.java
+++ skynet/src/main/java/com/bakery/skynet/SkynetApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SkynetApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkynetApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SkynetApplication.class, args);
+    }
 
 }
