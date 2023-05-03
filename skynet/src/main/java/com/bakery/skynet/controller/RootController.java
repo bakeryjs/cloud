f1005be0efaf1c48a8732a2e3e44f78e29d313ea
yehorbk <yehor.bk@gmail.com>
2023-05-03 17:18:31 +0300

    RootController error handling added

--- skynet/src/main/java/com/bakery/skynet/controller/RootController.java
+++ skynet/src/main/java/com/bakery/skynet/controller/RootController.java
@@ -1,4 +1,27 @@
 package com.bakery.skynet.controller;
 
+import com.bakery.skynet.exception.InvalidCredentialsException;
+import com.bakery.skynet.exception.UserAlreadyExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class RootController {
+
+    @ExceptionHandler
+    public void handleInvalidCredentialsException(
+            final InvalidCredentialsException ex,
+            final HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.UNAUTHORIZED.value(), ex.getMessage());
+    }
+
+    @ExceptionHandler
+    public void handleUserAlreadyExistsException(
+            final UserAlreadyExistsException ex,
+            final HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    }
+
 }
