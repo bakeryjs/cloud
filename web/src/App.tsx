da1c44d677380657baaec563b21570815b672402
yehorbk <yehor.bk@gmail.com>
2023-05-12 20:14:48 +0300

    landing page designed

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./theme/main.scss";
+
 import Landing from "./Landing";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 
+
 function App() {
   return (
-    <div>
-      <h1>Application</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" Component={Landing} />
-          <Route path="/login" Component={Login} />
-          <Route path="/dashboard" Component={Dashboard} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Landing} />
+        <Route path="/login" Component={Login} />
+        <Route path="/dashboard" Component={Dashboard} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- web/src/Landing/Landing.tsx
+++ web/src/Landing/Landing.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Col, Container, Navbar, Row } from "react-bootstrap";
 
-interface Props {}
-
-export default function Landing ({}: Props) {
-  return <div>Landing</div>
+export default function Landing() {
+  return (
+    <div className="landing">
+      <Navbar>
+        <Container>
+          <Navbar.Brand href="#">Bakery Cloud</Navbar.Brand>
+          <Navbar.Text className="sign-controls">
+            <a href="/login">Sign In</a>
+            <span> / </span>
+            <a href="/login">Sign Up</a>
+          </Navbar.Text>
+        </Container>
+      </Navbar>
+      <Container className="promotion-message">
+        <Row>
+          <Col className="text-center">
+            <h2>The cloud has never been so accessible</h2>
+            <span className="text-muted">
+              Try it yourself{" "}
+              <a className="link-dark" href="/dashboard">
+                now
+              </a>
+            </span>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
