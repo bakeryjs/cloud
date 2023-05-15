952a53108a26c24f1469731d0717a15e11e62991
yehorbk <yehor.bk@gmail.com>
2023-05-15 20:25:19 +0300

    cors configuration update

--- skynet/src/main/java/com/bakery/skynet/security/SecurityConfig.java
+++ skynet/src/main/java/com/bakery/skynet/security/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .cors().disable()
+                .cors().and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
